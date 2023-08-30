a0af832f55d80828a36c5c735096cd9887f9edc2
THEKINGSTAR <thekingstar5002@gmail.com>
2023-08-30 15:00:54 +0300

    fix parent childern relation HOPE THE LIVE HAPY :)

---
+++ test_code/1-binary_tree_insert_left.c
@@ -0,0 +1,43 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a node as the left-child of another node
+ * Where
+ * @parent: is a pointer to the node to insert the left-child in
+ * And
+ * @value: is the value to store in the new node
+ * Your function must
+ * Return: pointer to the created node,
+ * or
+ * NULL on failure or if parent is NULL
+ *
+ * If parent already has a left-child, the new node must take its place,
+ * and
+ * the old left-child must be set as the left-child of the new node.
+ *
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *ptr;
+
+	ptr = malloc(sizeof(binary_tree_t));
+	if (ptr == NULL || !ptr || parent == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		ptr->left = parent->left;
+		parent->left->parent = ptr;
+		parent->left = ptr;
+	}
+	else
+	{
+		parent->left = ptr;
+		ptr->left = NULL;
+	}
+	ptr->parent = parent;
+	ptr->right = NULL;
+	ptr->n = value;
+	return (ptr);
+
+}
