5db7dc6eba36c278e2a258427e6b99e961dc83cd
THEKINGSTAR <thekingstar5002@gmail.com>
2023-09-02 08:31:37 +0300

    9. Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,19 +9,15 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t r_height, l_height; /* ,height;*/
+	int r_height = 0, l_height = 0, height = 0;
 
 	if (tree == NULL)
 		return (0);
-
-	r_height = binary_tree_height(tree->right);
+	if (tree->left == NULL && tree->right == NULL)
+		return (l_height > r_height ? l_height : r_height);
 	l_height = binary_tree_height(tree->left);
+	r_height = binary_tree_height(tree->right);
 
-	/* height = (r_height > l_height) ? 1 + r_height : 1 + l_height; */
-	/* return (height); */
-
-	if (l_height > r_height)
-		return (l_height + 1);
-	else
-		return (r_height + 1);
+	height = (l_height > r_height ? l_height + 1 : r_height + 1);
+	return (height);
 }
